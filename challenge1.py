e77130be06ab1a768a3d34d2dbfd510153b5adef
Sanardi <rubypromise@gmx.com>
2020-11-22 20:39:13 +0000

    Add files via upload

--- challenge1.py
+++ challenge1.py
@@ -2,20 +2,20 @@
 import pandas as pd
 
 logfile = pd.read_table("input-file-10000.txt", delim_whitespace=True, header=None)
-
+#I a loading here into a Pandas Dataframe which is a fast way to parse through csv files.
 
 logfile.columns = ["Timestamp", "hostname1", "hostname2"]
-
+#renamed the columns just for easier code-reading
 
 def return_hostnames(logfile, init_datetime, end_datetime, hostname):
     
 
-   
     logs = logfile[logfile["Timestamp"] > init_datetime]
     logs = logfile[logfile["Timestamp"] < end_datetime]
     logs_to_host = logs[logs["hostname2"] == hostname]
             
     return print(logs_to_host['hostname1'].tolist())
+    #return the list of those hosts which have connected to the given server in the given period
 
-
+#this is a test:
 return_hostnames(logfile,1565647204351, 1565733592699, "Shealee")
